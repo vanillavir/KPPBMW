cdd945fa3c5f40f0c16958283af2c7de27cf7950
vanillavir <144926307+vanillavir@users.noreply.github.com>
2023-09-13 21:32:48 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import numpy as np 
+import matplotlib.pyplot as plt
+
+st.header("KPP BMW Peminatan C")
+st.subheader("Vira Azzahra-5023221033")
+
+ecg1= np.loadtxt('Data ECG.txt', dtype = 'float')
+Ndata1 = len(ecg1)
+ECG = np.arange(Ndata1)
+st.line_chart(ecg1)
+plt.figure(figsize=(18,8))
+plt.plot(ECG, ecg1)
+st.pyplot(plt)
